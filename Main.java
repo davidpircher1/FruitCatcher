0ab19304f470d6e020f95257a8fe2c0f369c1591
davidpircher1 <davidalexander.pircher@gmail.com>
2025-04-26 23:34:35 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+import Frames.MenuFrame;
+
+/**
+ * Hlavna trieda pre spustenie aplikacie.
+ * Tento program vytvori a zobrazi hlavne menu aplikacie.
+ */
+public class Main {
+    /**
+     * Hlavna metoda na spustenie aplikacie.
+     * Vytvori instanciu hlavneho menu.
+     *
+     * @param args Argumenty z príkazovej riadky (nevyuzivane v tomto pripade).
+     */
+    public static void main(String[] args) {
+        new MenuFrame();
+    }
+}
